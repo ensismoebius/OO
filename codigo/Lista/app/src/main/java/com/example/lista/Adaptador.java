6e68a853f99647485336cc9bace4aca627824ebc
LucasKawe <lucas.figueira2@etec.sp.gov.br>
2024-04-25 21:02:55 -0300

    Novo projeto adicionado

---
+++ codigo/Lista/app/src/main/java/com/example/lista/Adaptador.java
@@ -0,0 +1,38 @@
+package com.example.lista;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adaptador extends RecyclerView.Adapter {
+    private final Context contexto;
+    private List<Pessoa> lista;
+
+    public Adaptador(List<Pessoa> lista, Context contexto) {
+        this.lista = lista;
+        this.contexto = contexto;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Item(LayoutInflater.from(contexto).inflate(R.layout.item_da_lista,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Item i = (Item) holder;
+
+        i.nome.setText(lista.get(position).nome);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.lista.size();
+    }
+}
---
+++ codigo/Lista/app/src/main/java/com/example/lista/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lista;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView listaDePessoas = findViewById(R.id.lstPessoas);
+        listaDePessoas.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Pessoa> minhasPessoas = new ArrayList<Pessoa>();
+
+        minhasPessoas.add(new Pessoa("Aga", "Silva", 2010));
+        minhasPessoas.add(new Pessoa("Ega", "Silva", 2010));
+        minhasPessoas.add(new Pessoa("Iga", "Silva", 2010));
+        minhasPessoas.add(new Pessoa("Oga", "Silva", 2010));
+        minhasPessoas.add(new Pessoa("Uga", "Silva", 2010));
+
+        listaDePessoas.setAdapter(new Adaptador(minhasPessoas, this));
+
+    }
+}
